389a832432aa02a2ecd1f58b76c9743676b4d0ba
Dulce Hernandez <dulce.hernandezc@gmail.com>
2018-10-06 21:22:48 -0500

    cryptocurrency ticker

---
+++ src/components/Ticker/Ticker.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import './Ticker.css';
+import Cryptocurrency from '../Cryptocurrency/Cryptocurrency';
+import axios from 'axios';
+
+class Ticker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {
+          id: "bitcoin",
+          name: "Bitcoin",
+          symbol: "BTC",
+          price_usd: "1",
+          percent_change_1h: "0",
+          percent_change_24h: "0",
+          percent_change_7d: "0",
+        },
+        {
+          id: "ethereum",
+          name: "Ethereum",
+          symbol: "ETH",
+          price_usd: "1",
+          percent_change_1h: "0",
+          percent_change_24h: "0",
+          percent_change_7d: "0",
+        },
+        {
+          id: "litecoin",
+          name: "Litecoin",
+          symbol: "LTC",
+          price_usd: "1",
+          percent_change_1h: "0",
+          percent_change_24h: "0",
+          percent_change_7d: "0",
+        }
+      ]
+    };
+  }
+
+  componentDidMount() {
+    this.fetchCryptocurrencyData();
+    this.interval = setInterval(() => this.fetchCryptocurrencyData(), 60 * 1000);
+  }
+
+  fetchCryptocurrencyData() {
+    axios.get("https://api.coinmarketcap.com/v1/ticker/?limit=10")
+      .then(response => {
+        const wanted = ["bitcoin", "ethereum", "litecoin"];
+        let result = response.data.filter(currency => wanted.includes(currency.id));
+        this.setState({ data: result});
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    let tickers = this.state.data.map((currency) =>
+      <Cryptocurrency data={currency} key={currency.id} />
+    );
+    return (
+      <div className="tickers-container">
+        <ul className="tickers">{tickers}</ul>
+        <p>Information updated every minute courtesy of coinmarketcap.com</p>
+      </div>
+    );
+  }
+}
+
+export default Ticker;
