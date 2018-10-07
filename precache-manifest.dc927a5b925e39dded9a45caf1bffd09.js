28b861275988481e8f066b270d564e290062ee7c
Dulce Hernandez <dulce.hernandezc@gmail.com>
2018-10-06 21:59:36 -0500

    Updates

---
+++ precache-manifest.dc927a5b925e39dded9a45caf1bffd09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae60de60870853289fc9",
+    "url": "/cryptcurrency_react/static/js/runtime~main.ae60de60.js"
+  },
+  {
+    "revision": "cf56c156b4d15d8c63c3",
+    "url": "/cryptcurrency_react/static/js/main.cf56c156.chunk.js"
+  },
+  {
+    "revision": "d071339d9c6b52ce3195",
+    "url": "/cryptcurrency_react/static/js/1.d071339d.chunk.js"
+  },
+  {
+    "revision": "cf56c156b4d15d8c63c3",
+    "url": "/cryptcurrency_react/static/css/main.36cf0409.chunk.css"
+  },
+  {
+    "revision": "f9189f2fb185764a3e0f47ade1fce6c2",
+    "url": "/cryptcurrency_react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/cryptcurrency_react/precache-manifest.dc927a5b925e39dded9a45caf1bffd09.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/cryptcurrency_react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
